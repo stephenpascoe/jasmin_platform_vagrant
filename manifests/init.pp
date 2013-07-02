532129fbf5b83ded96bc1fd24c7637043104d34e
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-07-02 15:58:29 +0100

    Configure RAL proxy and install dev tools

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,12 +3,32 @@ group { "puppet":
 }
  
 File { owner => 0, group => 0, mode => 0644 }
+
  
 file { '/etc/motd':
   content => "Welcome to the JASMIN Analysis Platform.
                 Managed by Puppet.\n"
 }
 
+# This is an adapted version of the default content with the RAL proxy
+# added.
+file { '/etc/yum.conf':
+  content => "
+[main]
+cachedir=/var/cache/yum/\$basearch/\$releasever
+keepcache=0
+debuglevel=2
+logfile=/var/log/yum.log
+exactarch=1
+obsoletes=1
+gpgcheck=1
+plugins=1
+installonly_limit=5
+distroverpkg=centos-release
+
+proxy=http://wwwcache.rl.ac.uk:8080
+"
+}
 # Install the EPEL6 RPM repository
 # This install path is internal to RAL but alternative public URLs exist.
 #!TODO: this doesn't set the repository priority.  
@@ -20,6 +40,20 @@ package { 'epel6':
 }
 
 
+# Test installing a non-local RPM
+package { 'lynx':
+  ensure => installed,
+  require => File['/etc/yum.conf']
+}
+
+# Unless we itemise each development package we have no option but to use
+# exec to install this group.
+exec { 'yum Group Install':
+  require => File['/etc/yum.conf'],
+  unless => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
+  command => '/usr/bin/yum -y groupinstall "Development tools"'
+}
+  
 #!TODO: this doesn't work yet because yumit.jc is refusing access
 #yumrepo { 'jasmin':
 #  baseurl => "http://yumit.jc.rl.ac.uk/yum/rhel6",
@@ -28,7 +62,7 @@ package { 'epel6':
 #  enabled => 1,
 #  priority => 10
 #}
-#
+
 #package { 'jasmin-sci-vm':
 #  ensure => installed
 #}
