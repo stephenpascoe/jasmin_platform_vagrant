b6fdb9bedd537b2c0cdb71021554134e9cc2ae56
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-07-07 00:32:42 +0100

    Cleaner proxy configuration

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,15 @@
+
+##############################################################################
+# Configuration
+
+# Set the http proxy here so that your VM can access the RPM repositories
+$http_proxy = "wwwcache.rl.ac.uk:8080"
+
+# Select your EPEL mirror
+$epel_url = 'http://mirrors.ukfast.co.uk/sites/dl.fedoraproject.org/pub/epel/6/x86_64'
+
+##############################################################################
+
 group { "puppet":
   ensure => "present",
 }
@@ -10,49 +22,29 @@ file { '/etc/motd':
                 Managed by Puppet.\n"
 }
 
-# This is an adapted version of the default content with the RAL proxy
-# added.
-file { '/etc/yum.conf':
-  content => "
-[main]
-cachedir=/var/cache/yum/\$basearch/\$releasever
-keepcache=0
-debuglevel=2
-logfile=/var/log/yum.log
-exactarch=1
-obsoletes=1
-gpgcheck=1
-plugins=1
-installonly_limit=5
-distroverpkg=centos-release
-
-proxy=http://wwwcache.rl.ac.uk:8080
-"
-}
 
 # Unless we itemise each development package we have no option but to use
 # exec to install this group.
 exec { 'yum Group Install':
-  require => File['/etc/yum.conf'],
   unless => '/usr/bin/yum grouplist "Development tools" | /bin/grep "^Installed Groups"',
   command => '/usr/bin/yum -y groupinstall "Development tools"'
 }
 
 yumrepo { 'epel':
-  baseurl => 'http://mirrors.ukfast.co.uk/sites/dl.fedoraproject.org/pub/epel/6/x86_64',
+  baseurl => $epel_url,
   cost => absent,
   descr => "Public EPEL6 Repository",
+  proxy => $http_proxy,
   enabled => 1,
   gpgcheck => 1,
   gpgkey => "file:///vagrant/rpm-gpg/RPM-GPG-KEY-EPEL-6",
-  require => File['/etc/yum.conf'],
 }
 
 yumrepo { 'jasmin_platform':
   baseurl => "http://dist.ceda.ac.uk/yumrepo",
   cost => absent,
   descr => "JASMIN RPM repository",
-  proxy => "_none_",
+  proxy => $http_proxy,
   enabled => 1,
   gpgcheck => 1,
   gpgkey => "file:///vagrant/rpm-gpg/JASMIN-RPM-GPG-KEY",
@@ -68,4 +60,4 @@ package { 'jasmin-sci-vm':
 # In order to run the ipython notebook demo disable the firewall
 service { 'iptables':
   ensure => stopped,
-  }
+}
